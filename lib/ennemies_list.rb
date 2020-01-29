59cf7a620ce14aa78b2c13ed43844b365b9693c0
thibautgallice <gt.thibaut@gmail.com>
2020-01-29 08:09:40 +0100

    app_3.rb v2

--- lib/ennemies_list.rb
+++ lib/ennemies_list.rb
@@ -1,13 +1,19 @@
 class EnnemiesList
-
+  attr_accessor :total_ennemies, :ennemies
   def initialize(n)
     ennemies = []
+    @total_ennemies = n
     for i in 1..n do
       ennemies << Player.new("fighter#{i}")
       puts "Fighter#{i} créé !"
       ennemies
     end 
-    ennemies
+    @ennemies = ennemies
+  end
+
+  def kill_fighter(fighter)
+    @ennemies
+
   end
 
 end
--- lib/game.rb
+++ lib/game.rb
@@ -1,8 +1,8 @@
 class Game
   attr_accessor :human_player, :ennemies
   def initialize(player, number_of_ennemies = 4)
-    @player = player
-    @ennemies = EnnemiesList.new(number_of_ennemies)
+    @player = HumanPlayer.new("#{player}")
+    @ennemies_list = EnnemiesList.new(number_of_ennemies)
   end
 
 end
